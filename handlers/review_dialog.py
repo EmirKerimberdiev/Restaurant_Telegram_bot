7c56fa04a4a9d12ea5011ac15ce1122b002fe7d1
EmirKerimberdiev <keremberdievemir43@gmail.com>
2024-10-15 18:58:39 +0600

    add Dop Dz

--- handlers/review_dialog.py
+++ handlers/review_dialog.py
@@ -4,6 +4,8 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 
 review_router = Router()
+list_of_clients = []
+list_tg_ids = []
 
 
 class RestourantReview(StatesGroup):
@@ -20,6 +22,12 @@ async def review_handler(message: types.Message, state: FSMContext):
     # Выставляем состояние диолога на review.name
     await state.set_state(RestourantReview.name)
     await message.answer("Как вас зовут?")
+
+    if message.from_user.id not in list_tg_ids:
+        list_of_clients.append(message.from_user.id)
+    else:
+        await state.clear()
+        await message.answer("Вы уже проходили опрос!")
 
 
 @review_router.message(Command('stop'))
@@ -38,14 +46,14 @@ async def review_name(message: types.Message, state: FSMContext):
 
 @review_router.message(RestourantReview.phone_number)
 async def review_name(message: types.Message, state: FSMContext):
-    await state.update_data(name=message.text)
+    await state.update_data(phone_number=message.text)
     await state.set_state(RestourantReview.visit_date)
     await message.answer("Дата вашего посещения нашего заведения -День/Месяц/Год-")
 
 
 @review_router.message(RestourantReview.visit_date)
 async def review_age(message: types.Message, state: FSMContext):
-    await state.update_data(age=message.text)
+    await state.update_data(visit_date=message.text)
     await state.set_state(RestourantReview.food_rating)
     kb = types.ReplyKeyboardMarkup(
         keyboard=[
@@ -64,7 +72,7 @@ async def review_age(message: types.Message, state: FSMContext):
 
 @review_router.message(RestourantReview.food_rating)
 async def review_age(message: types.Message, state: FSMContext):
-    await state.update_data(age=message.text)
+    await state.update_data(food_rating=message.text)
     await state.set_state(RestourantReview.cleanliness_rating)
     kb = types.ReplyKeyboardMarkup(
         keyboard=[
@@ -83,16 +91,18 @@ async def review_age(message: types.Message, state: FSMContext):
 
 @review_router.message(RestourantReview.cleanliness_rating)
 async def review_gender(message: types.Message, state: FSMContext):
-    await state.update_data(gender=message.text)
+    await state.update_data(cleanliness_rating=message.text)
     await state.set_state(RestourantReview.extra_comments)
     await message.answer("Это дополнительный комментарий здесь вы можете написать всё что угодно.")
 
 
 @review_router.message(RestourantReview.extra_comments)
 async def review_genre(message: types.Message, state: FSMContext):
-    await state.update_data(genre=message.text)
+    await state.update_data(extra_comments=message.text)
 
     data = await state.get_data()
-    print(data)
+    list_of_clients.append(data)
+    print(list_of_clients)
+
     await state.clear()
     await message.answer("Спасибр за пройденный опрос!!!")
